73f79f113fda2915b9f80799e51e6db9d67c54c5
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-16 20:48:07 -0500

    User route functional allowing for the creation, edit, deletion and search of user

--- models/User.js
+++ models/User.js
@@ -1,46 +1,49 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {}
 
 // define table columns and configuration
-User.init(
-    {
-      id: {
+User.init({
+    id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
-      },
-      username: {
+    },
+    username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
-      },
-      email: {
+    },
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-          isEmail: true
+            isEmail: true
         }
-      },
-      password: {
+    },
+    password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          len: [8,20]
+            len: [8, 20]
         }
-      }
-    },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
     }
-  );
+}, {
+    hooks: {
+
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+});
 
 module.exports = User;
