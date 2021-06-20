5cad0895339f142f8bde3a1006d41c9ed37ccd23
Emily Crook <Emily.A.Crook96@gmail.com>
2021-06-19 19:46:06 -0500

    start of vote

--- models/index.js
+++ models/index.js
@@ -1,13 +1,18 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create an association between post and user
-User.hasMany(Post, {
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
     foreignKey: 'user_id'
-})
+});
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-})
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id',
+});
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
